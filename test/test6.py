097c375539eda972a8ba713b3075085ceffb6926
michael11315 <michael11315@hotmail.com>
2016-04-07 17:38:28 +0800

    create img

--- test/test6.py
+++ test/test6.py
@@ -6,21 +6,21 @@ class GridWindow(QWidget):
 	
 	def __init__(self, parent = None):
 		super(GridWindow, self).__init__(parent)
+		self.QLabelList = []
 		self.count = 0
-		self.createGridLayout()
+		self.createLayout()
 		self.btnConnection()
 	
-	def createGridLayout(self):
+	def createLayout(self):
 		gl = QGridLayout()
-		gl.setSpacing(1)
-		gl.setMargin(1)
+		gl.setSpacing(2)
+		gl.setMargin(2)
 		
-		self.QLabelList = []
 		# grid layout for image
-		for y in range(6):
-			for x in range(30):
+		for y in range(3):
+			for x in range(5):
 				self.QLabelList.append(QLabel())
-				pixmap = QPixmap('cell.jpg')
+				pixmap = QPixmap('grey.jpg')
 				self.QLabelList[len(self.QLabelList)-1].setPixmap(pixmap)
 				self.QLabelList[len(self.QLabelList)-1].setScaledContents(True)
 				gl.addWidget(self.QLabelList[len(self.QLabelList)-1], y, x)
@@ -36,22 +36,49 @@ class GridWindow(QWidget):
 		vl.addLayout(gl)
 		vl.addLayout(hl)
 		
-		self.setLayout(vl)
+		#self.setLayout(vl)
+		
+		hl2 = QHBoxLayout()
+		hl2.addLayout(vl)
+		
+		line = QFrame()
+		line.setFrameShape(QFrame.VLine)
+		line.setFrameShadow(QFrame.Sunken)
+		hl2.addWidget(line)
+		
+		myToolbar = QToolBar() 
+		hl2.addWidget(myToolbar)
+		
+		extractAction = QAction(QIcon('../img/photo.jpg'), 'hahahaha', self)
+		#extractAction.triggered.connect(exit())
+		myToolbar.addAction(extractAction)
+		
+		extractAction2 = QAction(QIcon('photo.jpg'), 'hahahaha', self)
+		#extractAction.triggered.connect(exit())
+		myToolbar.addAction(extractAction2)
+		
+		self.setLayout(hl2)
+		
+		#self.addWidget(myToolbar)
+		#QMainWindow.addToolBar( Qt.LeftToolBarArea , myToolbar )
 	
 	def btnConnection(self):
 		self.btn1.clicked.connect(self.btn1_changeGrid)
 		self.btn2.clicked.connect(self.btn2_changeGrid)
 	
 	def btn1_changeGrid(self, text):
-		if self.count < len(self.QLabelList):
-			pixmap = QPixmap('black.jpg')
+		if self.count < 15:
+			pixmap = QPixmap('../img/black.jpg')
 			self.QLabelList[self.count].setPixmap(pixmap)
+			#self.QLabelList[self.count].setScaledContents(True)
+			#self.QLabelList[self.count].setText('1')
 			self.count += 1
 	
 	def btn2_changeGrid(self, text):
-		if self.count < len(self.QLabelList):
-			pixmap = QPixmap('red.jpg')
+		if self.count < 15:
+			pixmap = QPixmap('../img/red.jpg')
 			self.QLabelList[self.count].setPixmap(pixmap)
+			#self.QLabelList[self.count].setText('2')
 			self.count += 1
 		
 if __name__ == "__main__":
