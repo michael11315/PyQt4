e86d0a9ebea4149590bcde8627ea5b3c9e3495d1
michael11315 <michael11315@hotmail.com>
2016-04-11 17:07:06 +0800

    rename function name and reconstruct code

--- project/project.py
+++ project/project.py
@@ -15,11 +15,17 @@ class GridWindow(QWidget):
 	def __init__(self, parent = None):
 		super(GridWindow, self).__init__(parent)
 		self.count = 0
-		self.createWindows()
+		self.sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+		self.sizeWidth = 60
+		self.sizeWidth_btn = 70
+		self.sizeHeight_btn = 25
+		
+		self.UIcreate()
 	
-	def createWindows(self):
-		self.createGridLayout()
-		self.createBarForGrid()
+	def UIcreate(self):
+		self.UIcreate_GridLayout()
+		self.UIcreate_BarForGrid()
+		self.initialGlobalAttribute()
 		
 		left_vl = QVBoxLayout()
 		for i in range(4):
@@ -28,7 +34,7 @@ class GridWindow(QWidget):
 		
 		self.setLayout(left_vl)
 	
-	def createGridLayout(self):
+	def UIcreate_GridLayout(self):
 		# global data
 		self.grid_qframe = []
 		self.grid_gl = []
@@ -54,13 +60,8 @@ class GridWindow(QWidget):
 			
 			# set relationship
 			self.grid_qframe[i].setLayout(self.grid_gl[i])
-			
-			# set attribute
-			self.grid_gl[i].setSpacing(1)
-			self.grid_gl[i].setMargin(1)
-			self.grid_qframe[i].setStyleSheet('background-color: gray;')
 	
-	def createBarForGrid(self):
+	def UIcreate_BarForGrid(self):
 		# full bar global data
 		self.bar_hl = []
 		for i in range(4):
@@ -83,17 +84,6 @@ class GridWindow(QWidget):
 			self.tbar_qframe[i].setLayout(self.tbar_hl[i])
 			self.bar_hl[i].addWidget(self.tbar_qframe[i])
 			self.bar_hl[i].addWidget(QFrame())
-			
-			# set attribute
-			#self.tbar_qframe[i].setStyleSheet('''.QFrame {border: 1px solid gray;}''')
-			self.tbar_hl[i].setSpacing(1)
-			self.tbar_hl[i].setMargin(1)
-			#self.tbar_qlabel[i].setAlignment(Qt.AlignCenter)
-		
-		self.tbar_qlabel[0].setText(self.tr('下局預測  大路'))
-		self.tbar_qlabel[1].setText(self.tr('下局預測  眼路'))
-		self.tbar_qlabel[2].setText(self.tr('下局預測  小路'))
-		self.tbar_qlabel[3].setText(self.tr('下局預測  筆路'))
 		
 		# left bar's global data
 		#----------------------------------------------------
@@ -101,29 +91,22 @@ class GridWindow(QWidget):
 		self.lbar_hl = []
 		self.lbar_qlabel = []
 		self.lbar_btn = []
-		self.lbar_qspinbox = []
+		self.lbar_qlineedit = []
 		
 		for i in range(4):
 			# initial left bar
 			self.lbar_qframe.append(QFrame())
 			self.lbar_hl.append(QHBoxLayout())
-			self.lbar_qlabel.append(QLabel(self.tr('莊')))
-			self.lbar_btn.append(QPushButton(self.tr('手動')))
-			self.lbar_qspinbox.append(QSpinBox())
+			self.lbar_qlabel.append(QLabel())
+			self.lbar_btn.append(QPushButton())
+			self.lbar_qlineedit.append(QLineEdit())
 			
 			# set relationship
 			self.lbar_hl[i].addWidget(self.lbar_qlabel[i])
 			self.lbar_hl[i].addWidget(self.lbar_btn[i])
-			self.lbar_hl[i].addWidget(self.lbar_qspinbox[i])
+			self.lbar_hl[i].addWidget(self.lbar_qlineedit[i])
 			self.lbar_qframe[i].setLayout(self.lbar_hl[i])
 			self.bar_hl[i].addWidget(self.lbar_qframe[i])
-			
-			# set attribute
-			self.lbar_qframe[i].setStyleSheet('''.QFrame {background-color: rgb(255, 0, 0); border: 1px solid gray;}''')
-			self.lbar_hl[i].setSpacing(1)
-			self.lbar_hl[i].setMargin(1)
-			self.lbar_qlabel[i].setStyleSheet('color: rgb(255, 255, 255);')
-			self.lbar_qlabel[i].setAlignment(Qt.AlignCenter)
 		
 		# right bar's global data
 		#----------------------------------------------------
@@ -137,9 +120,9 @@ class GridWindow(QWidget):
 			# initial right bar
 			self.rbar_qframe.append(QFrame())
 			self.rbar_hl.append(QHBoxLayout())
-			self.rbar_qlabel1.append(QLabel(self.tr('小計 : ')))
-			self.rbar_btn.append(QPushButton(self.tr('切停')))
-			self.rbar_qlabel2.append(QLabel(self.tr('合計 : ')))
+			self.rbar_qlabel1.append(QLabel())
+			self.rbar_btn.append(QPushButton())
+			self.rbar_qlabel2.append(QLabel())
 			
 			# set relationship
 			self.rbar_hl[i].addWidget(self.rbar_qlabel1[i])
@@ -147,14 +130,84 @@ class GridWindow(QWidget):
 			self.rbar_hl[i].addWidget(self.rbar_qlabel2[i])
 			self.rbar_qframe[i].setLayout(self.rbar_hl[i])
 			self.bar_hl[i].addWidget(self.rbar_qframe[i])
+	
+	def UIcreate_BetStatus(self):
+		self.bet_vl = QVBoxLayout()
+		
+		# bet and print area
+		#----------------------------------------------------
+		self.bbet = QGridLayout()
+		
+	
+	def initialGlobalAttribute(self):
+		# initail global values of UIcreate_GridLayout
+		#----------------------------------------------------
+		for i in range(4):
+			self.grid_qframe[i].setStyleSheet('background-color: gray;')
 			
-			# set attribute
+			self.grid_gl[i].setSpacing(1)
+			self.grid_gl[i].setMargin(1)
+		
+		# initail global values of UIcreate_BarForGrid
+		#----------------------------------------------------
+		# bar's title
+		#--------------------------
+		for i in range(4):
+			self.tbar_hl[i].setSpacing(1)
+			self.tbar_hl[i].setMargin(1)
+		
+		self.tbar_qlabel[0].setText(self.tr('下局預測  大路'))
+		self.tbar_qlabel[1].setText(self.tr('下局預測  眼路'))
+		self.tbar_qlabel[2].setText(self.tr('下局預測  小路'))
+		self.tbar_qlabel[3].setText(self.tr('下局預測  筆路'))
+		
+		# left bar
+		#--------------------------
+		for i in range(4):
+			self.lbar_qframe[i].setStyleSheet('''.QFrame {background-color: rgb(255, 0, 0); border: 1px solid gray;}''')
+			self.lbar_qframe[i].setSizePolicy(self.sizePolicy)
+			
+			self.lbar_hl[i].setSpacing(1)
+			self.lbar_hl[i].setMargin(1)
+			
+			self.lbar_qlabel[i].setText(self.tr('莊'))
+			self.lbar_qlabel[i].setStyleSheet('color: rgb(255, 255, 255);')
+			self.lbar_qlabel[i].setAlignment(Qt.AlignCenter)
+			self.lbar_qlabel[i].setSizePolicy(self.sizePolicy)
+			self.lbar_qlabel[i].setFixedWidth(self.sizeWidth)
+			
+			self.lbar_btn[i].setText(self.tr('手動'))
+			self.lbar_btn[i].setSizePolicy(self.sizePolicy)
+			self.lbar_btn[i].setFixedWidth(self.sizeWidth_btn)
+			self.lbar_btn[i].setFixedHeight(self.sizeHeight_btn)
+			
+			self.lbar_qlineedit[i].setFixedWidth(self.sizeWidth)
+			self.lbar_qlineedit[i].setSizePolicy(self.sizePolicy)
+		
+		# right bar
+		#--------------------------
+		for i in range(4):
 			self.rbar_qframe[i].setStyleSheet('''.QFrame {border: 1px solid gray;}''')
+			self.rbar_qframe[i].setSizePolicy(self.sizePolicy)
+			
 			self.rbar_hl[i].setSpacing(1)
 			self.rbar_hl[i].setMargin(1)
+			
+			self.rbar_qlabel1[i].setText(self.tr('小計 : '))
 			self.rbar_qlabel1[i].setAlignment(Qt.AlignCenter)
+			self.rbar_qlabel1[i].setSizePolicy(self.sizePolicy)
+			self.rbar_qlabel1[i].setFixedWidth(self.sizeWidth)
+			
+			self.rbar_btn[i].setText(self.tr('切停'))
+			self.rbar_btn[i].setSizePolicy(self.sizePolicy)
+			self.rbar_btn[i].setFixedWidth(self.sizeWidth_btn)
+			self.rbar_btn[i].setFixedHeight(self.sizeHeight_btn)
+			
+			self.rbar_qlabel2[i].setText(self.tr('合計 : '))
 			self.rbar_qlabel2[i].setAlignment(Qt.AlignCenter)
-		
+			self.rbar_qlabel2[i].setSizePolicy(self.sizePolicy)
+			self.rbar_qlabel2[i].setFixedWidth(self.sizeWidth)
+
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
 	GridWindow = GridWindow()
--- test/test7.py
+++ test/test7.py
@@ -9,3 +9,7 @@ setStyleSheet('background-color: rgb(0, 85, 255);')
 
 # set alignment
 self.tbar_qlabel[i].setAlignment(Qt.AlignCenter)
+
+# set sizePolicy
+sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+setSizePolicy(sizePolicy)
