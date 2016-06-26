81e631e9b6b1c5744191cb55b130dd7b64665ecb
michael11315 <michael11315@hotmail.com>
2016-06-26 23:32:51 +0800

    add ontrail new test

--- project/project.py
+++ project/project.py
@@ -2670,17 +2670,51 @@ def pressed(widget):
 	return filter.clicked
 
 def onTrail():
+	path = 'C:/ProgramData/recordMic'
+	if not os.path.exists(path):
+		os.mkdir(path)
+		with open(path + '/log', 'w') as file:
+			file.write('good 1 c')
+	else:
+		tmp =''
+		with open(path + '/log', 'r') as file:
+			for line in file:
+				if line.startswith('good'):
+					line = line.split()
+					count = int(line[1]) + 1
+					tmp = '%s %s %s' % (line[0], count, line[2])
+		
+		with open(path + '/log', 'w') as file:
+			file.write(tmp)
+	
+	if os.path.exists(path):
+		with open(path + '/log', 'r') as file:
+			fine = True
+			for line in file:
+				print line
+				if line.startswith('good'):
+					line = line.split()
+					if line[2] == 'r':
+						fine = False
+					elif int(line[1]) > 1000:
+						fine = False
+			if not fine:
+				return False
+	
 	timeNow = time.strftime("%Y %m %d %H", time.localtime(time.time())).split()
 	yearNow = int(timeNow[0])
 	monthNow = int(timeNow[1])
 	dayNow = int(timeNow[2])
 	hourNow = int(timeNow[3])
 	
-	if yearNow == 2016 and monthNow == 5 and dayNow <= 31 and dayNow >= 30:
+	if yearNow == 2016 and monthNow == 6 and dayNow <= 30 and dayNow >= 26:
 		return True
-	elif yearNow == 2016 and monthNow == 6 and dayNow <= 25 and dayNow >= 1:
+	elif yearNow == 2016 and monthNow == 7 and dayNow <= 3 and dayNow >= 1:
 		return True
 	else:
+		if os.path.exists(path):
+			with open(path + '/log', 'w') as file:
+				file.write('good 10000 r')
 		return False
 
 if __name__ == "__main__":
